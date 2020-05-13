cbfcb7d62c1d86d93b55797045399fb610f77244
Buschi2208 <simonbusch2208@gmail.com>
2020-05-12 20:58:37 -0400

    added overview

--- quizzes/views.py
+++ quizzes/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'quizzes/home.html')
+
+def overview(request):
+    return render(request,'quizzes/overview.html')
